57512c5eed6b0b42b1b2849c76ae2d5124c0d252
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-05-19 01:37:52 +0600

    task delete done

--- src/componants/About/About.js
+++ src/componants/About/About.js
@@ -3,13 +3,33 @@ import { useForm } from "react-hook-form";
 
 const About = () => {
     const [tasks, setTasks] = useState([]);
+    const [relode, setRelode] = useState(false);
 
     useEffect(() => {
         const url = `http://localhost:5000/users`;
         fetch(url)
             .then(res => res.json())
-            .then(data => setTasks(data))
-    }, []);
+            .then(data => {
+                setTasks(data);
+                setRelode(!relode);
+            })
+    }, [relode]);
+
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure?');
+        if (proceed) {
+            const url = `http://localhost:5000/users/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const rest = tasks.filter(task => task._id !== id);
+                    setTasks(rest);
+                })
+        }
+    }
 
     const { register, handleSubmit } = useForm();
     const onSubmit = (data, event) => {
@@ -29,15 +49,29 @@ const About = () => {
             })
     }
     return (
-        <div className='w-1/4 mx-auto mt-4'>
-            <form onSubmit={handleSubmit(onSubmit)} className='p-2 border-4'>
+        <div className='px-12'>
+            <form onSubmit={handleSubmit(onSubmit)} className='p-2 border-4 w-1/4 mx-auto mt-4'>
                 <input type="text" placeholder="Name" className="input input-bordered mb-3 w-full max-w-xs" {...register("name", { required: true })} /> <br />
                 <textarea placeholder="Description" className="input input-bordered mb-3 w-full max-w-xs" {...register("description", { required: true })} /> <br />
-                <input className='py-2 px-6 text-white font-bold rounded cursor-pointer block mx-auto bg-slate-500' type="submit" value="Task Add" />
+                <input className='py-2 px-6 text-white font-bold rounded cursor-pointer block mx-auto bg-slate-500' type="submit" value="Add Task" />
 
             </form>
-            <div className='mt-4'>
-                <h3 className='text-2xl text-indigo-600 font-bold text-center'>Task: {tasks.length}</h3>
+            <div className='mt-4 py-5'>
+                <h3 className='text-2xl text-indigo-600 font-bold text-center'>Total Task: {tasks.length}</h3>
+                <div>
+                    {
+                        tasks.map((task) => <div key={task._id} task={task} class="overflow-x-auto lg:w-3/4 mx-auto">
+                            <table className="table w-full">
+                                <tbody>
+                                    <tr className=''>
+                                        <td className='text-left ml-0 pl-0'><p>Name: {task.name}</p> <p>Description: {task.description}</p></td>
+                                        <td onClick={() => handleDelete(task._id)} className='text-right text-2xl text-red-600 font-bold cursor-pointer'>X</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>)
+                    }
+                </div>
             </div>
         </div>
     );
